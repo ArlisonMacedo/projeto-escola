4c1dd6bdcbc634b5b9ee8f31262f668b954b0332
ArlisonMacedo <arlisonmacedo526@gmail.com>
2019-11-04 19:58:24 -0300

    Pasta para Rotas

---
+++ Routes/insertFunc.php
@@ -0,0 +1,21 @@
+<?php
+
+    include '../Class/Crud.php';
+    
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $senha = $_POST['senha'];
+
+    if(preg_match('/^[A-Z]{1}[a-z0-9@]{5,}$/',$nome) && 
+       preg_match('/^[\d]{3}\.[\d]{3}\.[\d]{3}-[\d]{2}$/',$cpf) &&
+       preg_match('/^[\w@]{6,}$/',$senha)){
+        include '../Class/Func.php';
+        $func = new Func;
+        $func->setNome($nome);
+        $func->setCpf($cpf);
+        $func->setSenha($senha);
+        $crud = new Crud;
+        $crud->insertFunc($func);
+    }
+
+?>
---
+++ Routes/login.php
@@ -0,0 +1,21 @@
+<?php 
+
+    include '../Class/Crud.php';
+
+    $usuario = $_POST['nome'];
+    $senha = $_POST['senha'];
+
+    if(preg_match('/^[A-Z]{1}[a-z0-9@]{5,}$/',$usuario) && 
+       preg_match('/^[\w@]{6,}$/',$senha)){
+        
+        include '../Class/Func.php';
+        $func = new Func;
+        $func->setNome($usuario);
+        $func->setSenha($senha);
+        $crud = new Crud;
+        $crud->login($func);
+        
+       }
+
+
+?>
