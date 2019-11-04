9748f0f7ea442b9f96a780ff3eda60c893e6e737
ArlisonMacedo <arlisonmacedo526@gmail.com>
2019-11-04 20:00:05 -0300

    Index Page

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+    
+include 'Class/Crud.php';    
+if(isset($_SESSION['NOME'])){
+    echo $_SESSION['NOME'];
+}else{
+    echo "Entre";
+}
+
+?>
+
+<?php include 'Includes/Header.html';?>
+    <!--Todo codigo inicia Aqui -->
+    <form action="Routes/insertFunc.php" method="post">
+        <!-- Matricula <input type="text" name="mat" id=""><br> -->
+        Nome <input type="text" name="nome" id=""><br>
+        cpf <input type="text" name="cpf" id=""><br>
+        Senha <input type="password" name="senha" id="">
+        <input type="submit" value="Cadastrar">
+    </form>
+    <a href="Includes/Destroy.php">Sair</a>
+    <!--Todo codigo finaliza Aqui -->
+<?php include 'Includes/Footer.html'; ?>
